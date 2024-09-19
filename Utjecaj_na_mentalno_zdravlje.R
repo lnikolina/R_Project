f43a9e84587e5ce1beb2aad8d42a6c7f734c9a72
Nikolina Lekaj <nikolina.lekaj12@gmail.com>
2024-09-19 21:19:25 +0200

    Analiza opce anksioznosti i graf

--- Utjecaj_na_mentalno_zdravlje.R
+++ Utjecaj_na_mentalno_zdravlje.R
@@ -1,32 +1,31 @@
-# Izračunaj prosječnu razinu anksioznosti
-prosjek_anksioznosti <- mean(df$`Utjecaj na mentalno zdravlje:\nOsjećate li povećanu anksioznost zbog stalnih obavijesti koje pristižu s Vašeg mobilnog uređaja?`, na.rm = TRUE)
 
-# Ispis prosječne razine anksioznosti
-print(prosjek_anksioznosti)
+install.packages("readxl")
+install.packages("ggplot2")
+install.packages("dplyr")
 
-
-
-df$anksioznost <- as.numeric(factor(df$`Utjecaj na mentalno zdravlje:\nOsjećate li povećanu anksioznost zbog stalnih obavijesti koje pristižu s Vašeg mobilnog uređaja?`,
-                                    levels = c("Nimalo", "Malo", "Neutralan/a", "Dosta", "Veoma"),
-                                    labels = c(1, 2, 3, 4, 5)))
-
-df$ucestalost_obavijesti <- as.numeric(factor(df$`Koliko često provjeravate mobilne obavijesti tijekom dana?`,
-                                              levels = c("Nikada", "Rijetko", "Ponekad", "Često", "Stalno"),
-                                              labels = c(1, 2, 3, 4, 5)))
-
-df$opci_stav <- as.numeric(factor(df$`Kako biste opisali svoj opći stav prema mobilnim obavijestima?`,
-                                  levels = c("Pozitivan", "Neutralan", "Negativan"),
-                                  labels = c(1, 2, 3)))
-
-df$utjecaj_tehnologije <- as.numeric(factor(df$`Opći utjecaj tehnologije:\nSmatrate li da bi Vaša produktivnost bila veća kada biste u potpunosti isključili obavijesti tijekom rada?`,
-                                            levels = c("Da", "Ne", "Nisam siguran/a"),
-                                            labels = c(1, 2, 3)))
+library(readxl)
+library(ggplot2)
+library(dplyr)
 
 
-relevant_columns <- c("Utjecaj na mentalno zdravlje:\nOsjećate li povećanu anksioznost zbog stalnih obavijesti koje pristižu s Vašeg mobilnog uređaja?",
-                      "Koliko često provjeravate mobilne obavijesti tijekom dana?",
-                      "Kako biste opisali svoj opći stav prema mobilnim obavijestima?",
-                      "Opći utjecaj tehnologije:\nSmatrate li da bi Vaša produktivnost bila veća kada biste u potpunosti isključili obavijesti tijekom rada?")
+file_path <- "/Users/nikolinalekaj/Library/Mobile Documents/com~apple~CloudDocs/FIPU/HCI/PROJEKT/HCI (Responses).xlsx"
+data <- read_excel(file_path)
 
-Hipoteza2 <- df[, relevant_columns]
+
+colnames(data)
+
+
+# Seelektiranje
+selected_columns <- c(
+  "Utjecaj na mentalno zdravlje:\nOsjećate li povećanu anksioznost zbog stalnih obavijesti koje pristižu s Vašeg mobilnog uređaja?",
+  "Koliko često provjeravate mobilne obavijesti tijekom dana?",
+  "Kako biste opisali svoj opći stav prema mobilnim obavijestima?",
+  "Opći utjecaj tehnologije:\nSmatrate li da bi Vaša produktivnost bila veća kada biste u potpunosti isključili obavijesti tijekom rada?"
+)
+
+filtered_data <- data[, selected_columns]
+
+
+summary(filtered_data)
+
 
