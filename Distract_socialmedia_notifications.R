64fbbef4e18d229e0c528b5ee9a0447ed8751edc
Nikolina Lekaj <nikolina.lekaj12@gmail.com>
2024-09-19 14:14:21 +0200

    Analiza ometanja mobilnih obavijesti (drustvene mreze/ostalo)

---
+++ Distract_socialmedia_notifications.R
@@ -0,0 +1,42 @@
+
+library(readxl)
+
+# Učitanje Excel datoteke
+file_path <- "/Users/nikolinalekaj/Library/Mobile Documents/com~apple~CloudDocs/FIPU/HCI/PROJEKT/HCI (Responses).xlsx"
+df <- read_excel(file_path)
+
+# Filtriranje korisnike koji koriste društvene mreže
+drustvene_mreze_korisnici <- df[grepl("Društvene mreže", df$`Koje aplikacije najčešće koristite na svom pametnom telefonu?\n(Možete odabrati više odgovora.)`), ]
+
+# Pretvaranje Likertove skale u numeričke vrijednosti
+drustvene_mreze_korisnici$ometanje_numericko <- as.numeric(factor(
+  drustvene_mreze_korisnici$`Percepcija utjecaja mobilnih obavijesti:\nMolim Vas da označite u kojoj mjeri se slažete ili ne slažete s navedenim izjavama koristeći sljedeću skalu: ["Mobilne obavijesti me često ometaju tijekom rada."]`,
+  levels = c("Potpuno se ne slažem", "Ne slažem se", "Neutralan/a", "Slažem se", "Potpuno se slažem"),
+  labels = c(1, 2, 3, 4, 5)
+))
+
+# Izračun prosječne razine ometanja
+prosjek_ometanja <- mean(drustvene_mreze_korisnici$ometanje_numericko, na.rm = TRUE)
+
+# Ispis prosjeka ometanja
+print(prosjek_ometanja)
+
+
+
+
+# Filtriranje korisnika koji ne koriste društvene mreže
+ostali_korisnici <- df[!grepl("Društvene mreže", df$`Koje aplikacije najčešće koristite na svom pametnom telefonu?\n(Možete odabrati više odgovora.)`), ]
+
+# Pretvaranje Likertove skale u numeričke vrijednosti za ostale korisnike
+ostali_korisnici$ometanje_numericko <- as.numeric(factor(
+  ostali_korisnici$`Percepcija utjecaja mobilnih obavijesti:\nMolim Vas da označite u kojoj mjeri se slažete ili ne slažete s navedenim izjavama koristeći sljedeću skalu: ["Mobilne obavijesti me često ometaju tijekom rada."]`,
+  levels = c("Potpuno se ne slažem", "Ne slažem se", "Neutralan/a", "Slažem se", "Potpuno se slažem"),
+  labels = c(1, 2, 3, 4, 5)
+))
+
+# Izračun prosječne razine ometanja za ostale korisnike
+prosjek_ometanja_ostali <- mean(ostali_korisnici$ometanje_numericko, na.rm = TRUE)
+
+# Ispis prosjeka ometanja za ostale korisnike
+print(prosjek_ometanja_ostali)
+
